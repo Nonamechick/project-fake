503c1ad243c2e8221a24f27531365e1315349fca
Nonamechick <otajonovxasan350@gmail.com>
2025-03-05 22:59:21 +0530

    1 lesson done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import HiddenSearchBar from './components/HiddenSearchBar'
+// import ToggleBackgroundColor from './components/ToggleBackgroundColor'
+//import Calculator from './components/Calculator'
+//import Meals from './components/Meals'
+//import Todo from './components/Todo'
+//import Counter from './components/Counter'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      {/* <Counter /> 
+      <Todo />
+      <Meals /> 
+      <Calculator /> 
+      <ToggleBackgroundColor /> */}
+      <HiddenSearchBar />
+    </div>
   )
 }
 
